28099e742f64361bd7412e16492788f22b21ed39
f145hka <f145hka@meyola-server.meyola.ru>
2019-04-09 16:23:32 +0300

    Add 1st template

---
+++ landing/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render, get_object_or_404
+
+# Create your views here.
+def home_view(request):
+    return render(request, 'index.html')
--- uitt_net/settings.py
+++ uitt_net/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'landing',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'uitt_net.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'landing/../corp_site/../templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
