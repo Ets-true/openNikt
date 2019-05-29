fb210b9b7c7166a5ac33e9ae28d9d6f33428aa6d
F145Hka <f145hka@mail.ru>
2019-05-29 21:32:06 +0300

    add deployment vars to settings

--- uitt_net/settings.py
+++ uitt_net/settings.py
@@ -26,6 +26,18 @@ SECRET_KEY = '1y-th#rb_#@k#0qhtbzg!5(2g#ww^qm4_))&c$dp)!a4##5$mh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+#SECURITY SETTINGS
+if DEBUG:
+    SECURE_HSTS_SECONDS = 3600
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_BROWSER_XSS_FILTER = True
+    #SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    SECURE_HSTS_PRELOAD = True
+    CSRF_COOKIE_SECURE = True
+    X_FRAME_OPTIONS = 'DENY'
+
 UNDER_CONSTRUCTION = False
 
 ALLOWED_HOSTS = ["192.168.10.182", "web.devel"]
