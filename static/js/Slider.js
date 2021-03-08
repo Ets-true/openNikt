d61d2ca656c1b135bff7450c050fcadfab864299
ets-true <clean.energy.flow@gmail.com>
2021-03-08 17:04:51 +0300

    comment

---
+++ static/js/Slider.js
@@ -0,0 +1,8 @@
+$('.slideshow__slider').slick({
+    slidesToShow: 1,
+    speed: 800,
+    nextArrow: '.slideshow__next',
+    prevArrow: '.slideshow__prev',
+    dots: false,
+    infinite: true
+    }); 
