42e8a5c32b07ae339e51fdd09df1756bc5dcd96c
ets-true <clean.energy.flow@gmail.com>
2021-03-10 17:10:09 +0300

    comment

--- static/js/Slider.js
+++ static/js/Slider.js
@@ -5,4 +5,15 @@ $('.slideshow__slider').slick({
     prevArrow: '.slideshow__prev',
     dots: false,
     infinite: true
-    }); 
+});
+
+
+$('.slideshow_2__slider_2').slick({
+     slidesToShow: 1,
+     speed: 800,
+     nextArrow: '.slideshow_2__next',
+     prevArrow: '.slideshow_2__prev',
+     dots: false,
+     infinite: true,
+     
+}); 
